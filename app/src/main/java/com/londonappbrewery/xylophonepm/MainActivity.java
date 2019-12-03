a8e5137540c302d44d9dad118921ba0e38579b8a
slier81 <slier81@gmail.com>
2019-12-03 11:30:23 +0800

    Finish with the app

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -17,29 +17,56 @@ public class MainActivity extends AppCompatActivity {
     private final float NORMAL_PLAY_RATE = 1.0f;
 
     // TODO: Add member variables here
-    private int mCSoundId;
-    private int mDSoundId;
-    private int mESoundId;
-    private int mFSoundId;
-    private int mGSoundId;
-    private int mASoundId;
-    private int mBSoundId;
+    private SoundPool soundPool;
+    private int cSoundId;
+    private int dSoundId;
+    private int eSoundId;
+    private int fSoundId;
+    private int gSoundId;
+    private int aSoundId;
+    private int bSoundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Create a new SoundPool
+        this.soundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
-
-        // TODO: Load and get the IDs to identify the sounds
-
-
+        this.cSoundId = this.soundPool.load(getApplicationContext(), R.raw.note1_c, PRIORITY);
+        this.dSoundId = this.soundPool.load(getApplicationContext(), R.raw.note2_d, PRIORITY);
+        this.eSoundId = this.soundPool.load(getApplicationContext(), R.raw.note3_e, PRIORITY);
+        this.fSoundId = this.soundPool.load(getApplicationContext(), R.raw.note4_f, PRIORITY);
+        this.gSoundId = this.soundPool.load(getApplicationContext(), R.raw.note5_g, PRIORITY);
+        this.aSoundId = this.soundPool.load(getApplicationContext(), R.raw.note6_a, PRIORITY);
+        this.bSoundId = this.soundPool.load(getApplicationContext(), R.raw.note7_b, PRIORITY);
     }
 
-    // TODO: Add the play methods triggered by the buttons
+    public void playA(View v) {
+        this.soundPool.play(this.aSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
+    public void playB(View v) {
+        this.soundPool.play(this.bSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
+    public void playC(View v) {
+        this.soundPool.play(this.cSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 
+    public void playD(View v) {
+        this.soundPool.play(this.dSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playE(View v) {
+        this.soundPool.play(this.eSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playF(View v) {
+        this.soundPool.play(this.fSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playG(View v) {
+        this.soundPool.play(this.gSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
 }
